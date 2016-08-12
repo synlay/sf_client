f24e3e1f2ea1f1856428b1cec38420efcd229702
David Robakowski <david.robakowski@synlay.com>
2016-08-12 22:19:55 +0200

    Implement sf_client:update/3 and fix restc patch method

--- src/sf_client.erl
+++ src/sf_client.erl
@@ -14,16 +14,14 @@
 %% API
 -export([
      create/2
-    ,update/2
+    ,update/3
     ,delete/2
 ]).
 
 
 create(MappingKey, Model) ->
     do([error_m ||
-        SObjectMapping <- sf_client_sobjects_mapping_server:get_sobjects_mapping(MappingKey),
-        MappingModule = sf_client_sobjects_mapping:'#get-model'(mapping_module, SObjectMapping),
-        MappingUrl = sf_client_sobjects_mapping:'#get-model'(mapping_url, SObjectMapping),
+        {MappingModule, MappingUrl} <- get_model_mapping_config(MappingKey),
         Response <- sf_client_lib:request(MappingModule:new_sobject_from_model(Model), post, 201, MappingUrl),
         Id <- sf_client_lib:undefined_lift(st_traverse_utils:traverse_by_path(<<"id">>, Response), error_m,
                                            no_id_attribute_found),
@@ -33,7 +31,14 @@ create(MappingKey, Model) ->
     ]).
 
 
-update(_MappingKey, _Model) -> sf_client_lib:request(undefined, patch, 204, undefined).
+update(MappingKey, SObjectId, Model) ->
+    do([error_m ||
+        {MappingModule, MappingUrl} <- get_model_mapping_config(MappingKey),
+        SObjectUrl = restc:construct_url(binary_to_list(MappingUrl), binary_to_list(SObjectId), []),
+        _ <- sf_client_lib:request(MappingModule:new_sobject_from_model(Model), patch, 204, SObjectUrl),
+        lager:notice("Successfully updated sobject with ID: '~ts' for model: '~s'",
+                     [SObjectId, MappingModule:to_string(Model)])
+    ]).
 
 
 delete(MappingKey, SObjectId) ->
@@ -48,3 +53,11 @@ delete(MappingKey, SObjectId) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+get_model_mapping_config(MappingKey) ->
+    do([error_m ||
+        SObjectMapping <- sf_client_sobjects_mapping_server:get_sobjects_mapping(MappingKey),
+        MappingModule = sf_client_sobjects_mapping:'#get-model'(mapping_module, SObjectMapping),
+        MappingUrl = sf_client_sobjects_mapping:'#get-model'(mapping_url, SObjectMapping),
+        return({MappingModule, MappingUrl})
+    ]).
